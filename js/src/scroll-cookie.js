859073f76ff3deb44069d2db3aab9e40b73d8bba
耿忠义 <gengzhy@yonyou.com>
2021-02-09 00:36:22 +0800

    Site updated: 2021-02-09 00:36:08

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat May 16 2020 12:22:29 GMT+0800 (GMT+08:00)
+// build time:Tue Feb 09 2021 00:36:02 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
